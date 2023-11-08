619fb63d4fc210bd12ee11e7a6ec0d9afd33b6e5
LUIS ALEJANDRO GIRON TELLO <tatotello210@gmail.com>
2023-11-08 16:36:37 -0600

    Creacion Tabla Temporal

--- SP.sql
+++ SP.sql
@@ -4,5 +4,15 @@ BEGIN
 	SET NOCOUNT ON; --No muestra las filas aceptadas	
 	SET TRAN ISOLATION LEVEL SERIALIZABLE	--Nivel de aislamiento ?
 
+	create table #tIngreso_Estudiantes(
+	Codigo_Estudiante varchar(20) not null,
+	Nombre varchar(20) not null,
+	Apellido varchar(20) not null,
+	Codigo_Seguridad varchar(20) not null,
+	Codigo_Curso varchar(20) not null
+)
+
+
+
 END
 
