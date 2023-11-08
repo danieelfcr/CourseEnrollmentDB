427928edda107c5a927f379afd1fbb75869376aa
danieelfcr <danielfcab2002@gmail.com>
2023-11-08 14:17:44 -0600

    SP structure added

---
+++ SP.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE uspUploadData  --Nombre del SP
+AS
+BEGIN
+	SET NOCOUNT ON; --No muestra las filas aceptadas	
+	SET TRAN ISOLATION LEVEL SERIALIZABLE	--Nivel de aislamiento ?
+
+END
+
