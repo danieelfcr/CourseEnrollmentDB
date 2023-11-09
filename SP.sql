b7addd31d0c8cb6953553157f25d0ef6b3bfc4cb
LUIS ALEJANDRO GIRON TELLO <tatotello210@gmail.com>
2023-11-08 21:47:33 -0600

    Implementacion CSV

--- SP.sql
+++ SP.sql
@@ -10,5 +10,20 @@ BEGIN
 	Codigo_Seguridad varchar(20) not null,
 	Curso_Asignar varchar(20) not null
 )
+begin try 
+    BULK INSERT #tIngreso_Estudiantes
+    FROM '‪C:\Input.csv'
+    WITH ( FORMAT = 'CSV',
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '\n',
+        FIRSTROW = 1 
+    );
+	end try
+	begin catch
+		print 'Ah ocurrido un error al cargar el archivo :/'
+	end catch
+
+
+
 END
 
