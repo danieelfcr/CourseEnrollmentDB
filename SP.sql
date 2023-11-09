553ccae6242c0394b35d5de4d0b84cc3e2f6a21d
LUIS ALEJANDRO GIRON TELLO <tatotello210@gmail.com>
2023-11-08 21:42:31 -0600

    Modificacion Tabla Temporal

--- SP.sql
+++ SP.sql
@@ -6,13 +6,9 @@ BEGIN
 
 	create table #tIngreso_Estudiantes(
 	Codigo_Estudiante varchar(20) not null,
-	Nombre varchar(20) not null,
-	Apellido varchar(20) not null,
+	Segundo_Apellido varchar(20) not null,
 	Codigo_Seguridad varchar(20) not null,
-	Codigo_Curso varchar(20) not null
+	Curso_Asignar varchar(20) not null
 )
-
-
-
 END
 
