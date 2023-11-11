b80368b307c56ffffb55cd6adda64be4fd47f767
danieelfcr <danielfcab2002@gmail.com>
2023-11-11 16:24:58 -0600

    Correccion de errores y codigo comentado

--- SP.sql
+++ SP.sql
@@ -4,6 +4,7 @@ BEGIN
 	SET NOCOUNT ON; --No muestra las filas aceptadas	
 	SET TRAN ISOLATION LEVEL SERIALIZABLE	--Nivel de aislamiento ?
 
+	--Creacion tabla temporal para guardar contenido de CSV
 	create table #tIngreso_Estudiantes(
 	Codigo_Estudiante varchar(20) not null,
 	Segundo_Apellido varchar(20) not null,
@@ -11,6 +12,7 @@ BEGIN
 	Curso_Asignar varchar(20) not null
 )
 begin try 
+	--Insertar data de CSV a tabla
     BULK INSERT #tIngreso_Estudiantes
     FROM '‪C:\Input.csv'
     WITH ( FORMAT = 'CSV',
@@ -42,7 +44,8 @@ begin try
 			@vPromedioMenor float,
 			@vPromedioActual float,
 			@vEstudianteMenor int
-
+	
+	--Recorrer tabla temporal con cursor
 	Declare cDatosAsign cursor for
 	select Codigo_Estudiante, Segundo_Apellido, Codigo_Seguridad, Curso_Asignar
 	from #tIngreso_Estudiantes
@@ -77,22 +80,25 @@ begin try
 				select TOP 1 @vSeccionTec2 = s.ID_Seccion, @vCupoTec2 = s.Cupo 
 				from Seccion s
 				where s.ID_Curso = @vCursoAsignacion
-				ORDER BY s.ID_Seccion OFFSET 1 ROW
+				ORDER BY s.ID_Seccion OFFSET 1 ROW	-- Tomar el segundo resultado con este comando
 				FETCH NEXT 1 ROW ONLY;
 
+				--Verificar que existan prerrequisitos en la tabla
 				if ((select count(1) from Prerrequisito cp where cp.Curso = @vCursoAsignacion and cp.Estado = 1) > 0)
 				BEGIN
-					if(dbo.fncEstudianteCumplePrerrequisitos = 1)
+					--Verificar que estudiante cumpla prerrequisitos
+					if(dbo.fncEstudianteCumplePrerrequisitos(@vCursoAsignacion, @vID_Estudiante) = 1)
 					begin
 						-- Verificar que hay cupos disponibles en la primera sección
 						if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec1) < @vCupoTec1)
 						begin
 							--set @SeccionTX = @vSeccionAdmin
-							set @SeccionTX = @vSeccionTec1
-							set @EstadoTx = 1
-							set @vFecha_AsignacionTX = GETDATE()	
+							set @vSeccionTX = @vSeccionTec1
+							set @vEstadoTx = 1
+							set @vFecha_AsignacionTX = GETDATE()	--Se asignó, por lo que se inserta la fecha asignación
 						end
 						else
+						--Si no hay cupos disponibles en la primera seccion
 						begin
 							WITH PromedioNotasPorEstudiante AS (
 							SELECT h.Estudiante, AVG(CAST(h.Nota AS FLOAT)) AS PromedioNotas
@@ -108,74 +114,83 @@ begin try
 							GROUP BY h.Estudiante
 							)
 
+							--Tomar al estudiante con promedio menor de seccion 1
 							SELECT TOP 1 @vPromedioMenor = p.PromedioNotas, @vEstudianteMenor = p.Estudiante
 							FROM	PromedioNotasPorEstudiante p
 							ORDER BY p.PromedioNotas ASC;
 
-							SELECT @PromedioActual = AVG(CAST(Nota AS FLOAT))
+							--Calcular promedio actual del estudiante intentando asignarse
+							SELECT @vPromedioActual = AVG(CAST(Nota AS FLOAT))
 							FROM Historial_Cursos h
 							join Prerrequisito p on h.Curso = p.Prerrequisito
-							WHERE Estudiante = @ID_Estudiante and p.Curso = @vCursoAsignacion
+							WHERE Estudiante = @vID_Estudiante and p.Curso = @vCursoAsignacion
 
-							if(@PromedioActual <= @PromedioMenor)
+							--Si promedio actual es menor a promedio menor, asignar a seccion 2
+							if(@vPromedioActual <= @vPromedioMenor)
 							begin
 								if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec2) < @vCupoTec2)
 								begin
-									set @SeccionTX = @vSeccionTec2
-									set @EstadoTx = 1
+									set @vSeccionTX = @vSeccionTec2
+									set @vEstadoTx = 1
 									set @vFecha_AsignacionTX = GETDATE()
 								end
 								else
 								begin
-									set @SeccionTX = @vSeccionTec2
-									set @EstadoTx = 2
+									--Si no hay cupo en seccion 2, mandar a lista de espera (Estado 2)
+									set @vSeccionTX = @vSeccionTec2
+									set @vEstadoTx = 2
 								end
 							end
 							else
 							begin
+							--Si promedio actual es mayor a promedio menor, realizar intercambio
+								--Comprobar que exista cupo en seccion 2
 								if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec2) < @vCupoTec2)
 								begin
 									update tx_Asignacion
-											set Estado = 3
-											Seccion = @vSeccionTec2
+											set Estado = 3,
+											Seccion = @vSeccionTec2,
 											Fecha_Asignacion = GETDATE()
 										where Estudiante = @vEstudianteMenor
 
-										set @SeccionTX = @vSeccionTec1
-										set @EstadoTx = 1
+										set @vSeccionTX = @vSeccionTec1
+										set @vEstadoTx = 1
 										set @vFecha_AsignacionTX = GETDATE()
 								end
 								else
 								begin
+								--Si no hay cupo en seccion 2, mandar al de menor promedio a lista de espera y asignar al mayor a seccion 1
 									update tx_Asignacion
-									set Estado = 2
-										Seccion = @vSeccionTec2
+									set Estado = 2,
+										Seccion = @vSeccionTec2,
 										Fecha_Asignacion = null
 									where Estudiante = @vEstudianteMenor
 
-									set @SeccionTX = @vSeccionTec1
-									set @EstadoTx = 1
+									set @vSeccionTX = @vSeccionTec1
+									set @vEstadoTx = 1
 									set @vFecha_AsignacionTX = GETDATE()
 								end
 							end
-
-
-
 						end
 					end
 					else
 					begin
+					--Si estudiante no cumple prerrequisitos, el estado asignacion sera rechazado
 						set @vEstadoTx = 0
 					end
 				END
+				--Si no existen prerrequisitos
 				ELSE
 				BEGIN
+					--Verificar si hay cupo en la seccion 1
 					if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec1) < @vCupoTec1)
 					begin
-						set @SeccionTX = @vSeccionAdmin
-						set @EstadoTx = 1
+						--Si hay cupo, asignar a seccion 1
+						set @vSeccionTX = @vSeccionAdmin
+						set @vEstadoTx = 1
 						set @vFecha_AsignacionTX = GETDATE()	
 					end
+					--Si no hay cupo en la seccion 1, probar con la seccion 2
 					else
 					begin
 						WITH PromedioNotasPorEstudiante AS (
@@ -190,52 +205,60 @@ begin try
 						GROUP BY h.Estudiante
 						)
 
+						--Seleccionar al estudiante en seccion 1 con el menor promedio
 						SELECT TOP 1 @vPromedioMenor = p.PromedioNotas, @vEstudianteMenor = p.Estudiante
 						FROM	PromedioNotasPorEstudiante p
 						ORDER BY p.PromedioNotas ASC;
 
-						SELECT @PromedioActual = AVG(CAST(Nota AS FLOAT))
+						--Obtener promedio actual de estudiante intentando asignarse
+						SELECT @vPromedioActual = AVG(CAST(Nota AS FLOAT))
 						FROM Historial_Cursos
-						WHERE Estudiante = @ID_Estudiante;
+						WHERE Estudiante = @vID_Estudiante;
 
-						if(@PromedioActual <= @PromedioMenor)
+						if(@vPromedioActual <= @vPromedioMenor)
+						--Si promedio actual es menor o igual al promedio menor
 						begin
 							if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec2) < @vCupoTec2)
 							begin
-								set @SeccionTX = @vSeccionTec2
-								set @EstadoTx = 1
+								--Si hay cupo en la seccion 2, asignarlo
+								set @vSeccionTX = @vSeccionTec2
+								set @vEstadoTx = 1
 								set @vFecha_AsignacionTX = GETDATE()
 							end
 							else
 							begin
-								set @SeccionTX = @vSeccionTec2
-								set @EstadoTx = 2
+								--Si no hay cupo, mandarlo a lista de espera
+								set @vSeccionTX = @vSeccionTec2
+								set @vEstadoTx = 2
 							end
 						end
 						else
+						--Si promedio actual es mayor al promedio menor
 						begin
 							if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec2) < @vCupoTec2)
 							begin
+							--Si hay cupo en seccion 2, realizar intercambio de seccion
 								update tx_Asignacion
-										set Estado = 3
-										Seccion = @vSeccionTec2
+										set Estado = 3,
+										Seccion = @vSeccionTec2,
 										Fecha_Asignacion = GETDATE()
 									where Estudiante = @vEstudianteMenor
 
-									set @SeccionTX = @vSeccionTec1
-									set @EstadoTx = 1
+									set @vSeccionTX = @vSeccionTec1
+									set @vEstadoTx = 1
 									set @vFecha_AsignacionTX = GETDATE()
 							end
 							else
+							--Si no hay cupo en seccion 2, promedio menor a lista de espera y asignar al otro estudiante a sec 1
 							begin
 								update tx_Asignacion
-								set Estado = 2
-									Seccion = @vSeccionTec2
+								set Estado = 2,
+									Seccion = @vSeccionTec2,
 									Fecha_Asignacion = null
 								where Estudiante = @vEstudianteMenor
 
-								set @SeccionTX = @vSeccionTec1
-								set @EstadoTx = 1
+								set @vSeccionTX = @vSeccionTec1
+								set @vEstadoTx = 1
 								set @vFecha_AsignacionTX = GETDATE()
 							end
 						end
@@ -249,38 +272,48 @@ begin try
 				from Seccion s
 				where s.ID_Curso = @vCursoAsignacion
 
+				--Verificar si existen prerrequisitos
 				if ((select count(1) from Prerrequisito cp where cp.Curso = @vCursoAsignacion and cp.Estado = 1) > 0)
 				begin
-					if(dbo.fncEstudianteCumplePrerrequisitos = 1)
+					--Verificar que estudiante cumpla con prerrequisitos
+					if(dbo.fncEstudianteCumplePrerrequisitos(@vCursoAsignacion, @vID_Estudiante) = 1)
 					begin
+						--Verificar que exista cupo
 						if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionAdmin) = @vCupoAdmin)
 						begin
-							set @SeccionTX = @vSeccionAdmin
-							set @EstadoTx = 2
+							--Si el cupo esta lleno, mandar a lista de espera
+							set @vSeccionTX = @vSeccionAdmin
+							set @vEstadoTx = 2
 						end
 						else
 						begin
-							set @SeccionTX = @vSeccionAdmin
-							set @EstadoTx = 1
+							--Si hay cupo, asignar
+							set @vSeccionTX = @vSeccionAdmin
+							set @vEstadoTx = 1
 							set @vFecha_AsignacionTX = GETDATE()
 						end
 					end
 					else
 					begin
-						set @EstadoTx = 0
+						--Si estudiante no cumple prerrequisito, rechazar
+						set @vEstadoTx = 0
 					end
 				end
 				else
+				--Si no existen prerrequisitos
 				begin
+					--Comprobar que aun exista cupo
 					if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionAdmin) = @vCupoAdmin)
 					begin
-						set @SeccionTX = @vSeccionAdmin
-						set @EstadoTx = 2
+						--Si ya no hay cupo, mandar a lista de espera
+						set @vSeccionTX = @vSeccionAdmin
+						set @vEstadoTx = 2
 					end
 					else
 					begin
-						set @SeccionTX = @vSeccionAdmin
-						set @EstadoTx = 1
+						--Si hay cupo, asignar
+						set @vSeccionTX = @vSeccionAdmin
+						set @vEstadoTx = 1
 						set @vFecha_AsignacionTX = GETDATE()
 					end
 				end
@@ -294,12 +327,12 @@ begin try
 
 		--Insertar valores a tabla tx_asignacion
 		INSERT INTO tx_Asignacion (Fecha_Creacion, Fecha_Asignacion, Estudiante, Seccion, Estado)
-		VALUES (@Fecha_CreacionTX, @Fecha_AsignacionTX, @ID_Estudiante, @SeccionTX, @EstadoTX)
+		VALUES (@vFecha_CreacionTX, @vFecha_AsignacionTX, @vID_Estudiante, @vSeccionTX, @vEstadoTX)
 
 		END TRY
 
 		BEGIN CATCH
-
+			print('Ha ocurrido un error en el proceso')
 		END CATCH
 		
 	fetch next from cDatosAsign into @CodigoEstudiante, @vSegundoApellido, @vCodigoSeguridad, @vCursoAsignacion
