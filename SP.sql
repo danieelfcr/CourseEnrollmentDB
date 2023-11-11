5f67efec5d8aefb4c9cff1656f49dd459f3c6c3e
JoseMarold <jmmr114@gmail.com>
2023-11-10 20:14:03 -0600

    Modificaciones en el proceso de asignación de cursos en el SP

--- SP.sql
+++ SP.sql
@@ -68,10 +68,12 @@ begin try
 			IF ((SELECT COUNT(*) FROM Seccion s inner join Curso c on (c.ID_Curso = s.ID_Curso)) >= 2)
 			BEGIN
 				-- curso es de tipo tecnico
+				-- Se toma la información de la primera sección del curso
 				select TOP 1 @vSeccionTec1 = s.ID_Seccion, @vCupoTec1 = s.Cupo
 				from Seccion s
 				where s.ID_Curso = @vCursoAsignacion
 
+				-- Se toma la información de la segunda sección del curso
 				select TOP 1 @vSeccionTec2 = s.ID_Seccion, @vCupoTec2 = s.Cupo 
 				from Seccion s
 				where s.ID_Curso = @vCursoAsignacion
@@ -79,85 +81,95 @@ begin try
 				FETCH NEXT 1 ROW ONLY;
 
 				if ((select count(1) from Prerrequisito cp where cp.Curso = @vCursoAsignacion and cp.Estado = 1) > 0)
-				begin
+				BEGIN
 					if(dbo.fncEstudianteCumplePrerrequisitos = 1)
 					begin
+						-- Verificar que hay cupos disponibles en la primera sección
 						if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec1) < @vCupoTec1)
 						begin
-							set @SeccionTX = @vSeccionAdmin
+							--set @SeccionTX = @vSeccionAdmin
+							set @SeccionTX = @vSeccionTec1
 							set @EstadoTx = 1
 							set @vFecha_AsignacionTX = GETDATE()	
 						end
-					end
-					else
-					begin
-						WITH PromedioNotasPorEstudiante AS (
-						SELECT h.Estudiante, AVG(CAST(h.Nota AS FLOAT)) AS PromedioNotas
-						FROM Historial_Cursos h
-						join Prerrequisito p on h.Curso = p.Prerrequisito
-						WHERE EXISTS (
-							SELECT 1
-							FROM tx_Asignacion a
-							JOIN Seccion s ON a.Seccion = s.ID_Seccion
-							WHERE	a.Estudiante = h.Estudiante	AND s.ID_Curso = @vCursoAsignacion
-							)
-						and p.Curso = @vCursoAsignacion
-						GROUP BY h.Estudiante
-						)
-						SELECT TOP 1 @vPromedioMenor = p.PromedioNotas, @vEstudianteMenor = p.Estudiante
-						FROM	PromedioNotasPorEstudiante p
-						ORDER BY p.PromedioNotas ASC;
-
-						SELECT @PromedioActual = AVG(CAST(Nota AS FLOAT))
-						FROM Historial_Cursos h
-						join Prerrequisito p on h.Curso = p.Prerrequisito
-						WHERE Estudiante = @ID_Estudiante and p.Curso = @vCursoAsignacion
-
-						if(@PromedioActual <= @PromedioMenor)
+						else
 						begin
-							if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec2) < @vCupoTec2)
+							WITH PromedioNotasPorEstudiante AS (
+							SELECT h.Estudiante, AVG(CAST(h.Nota AS FLOAT)) AS PromedioNotas
+							FROM Historial_Cursos h
+							join Prerrequisito p on h.Curso = p.Prerrequisito
+							WHERE EXISTS (
+								SELECT 1
+								FROM tx_Asignacion a
+								JOIN Seccion s ON a.Seccion = s.ID_Seccion
+								WHERE	a.Estudiante = h.Estudiante	AND s.ID_Curso = @vCursoAsignacion
+								)
+							and p.Curso = @vCursoAsignacion
+							GROUP BY h.Estudiante
+							)
+
+							SELECT TOP 1 @vPromedioMenor = p.PromedioNotas, @vEstudianteMenor = p.Estudiante
+							FROM	PromedioNotasPorEstudiante p
+							ORDER BY p.PromedioNotas ASC;
+
+							SELECT @PromedioActual = AVG(CAST(Nota AS FLOAT))
+							FROM Historial_Cursos h
+							join Prerrequisito p on h.Curso = p.Prerrequisito
+							WHERE Estudiante = @ID_Estudiante and p.Curso = @vCursoAsignacion
+
+							if(@PromedioActual <= @PromedioMenor)
 							begin
-								set @SeccionTX = @vSeccionTec2
-								set @EstadoTx = 1
-								set @vFecha_AsignacionTX = GETDATE()
+								if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec2) < @vCupoTec2)
+								begin
+									set @SeccionTX = @vSeccionTec2
+									set @EstadoTx = 1
+									set @vFecha_AsignacionTX = GETDATE()
+								end
+								else
+								begin
+									set @SeccionTX = @vSeccionTec2
+									set @EstadoTx = 2
+								end
 							end
 							else
 							begin
-								set @SeccionTX = @vSeccionTec2
-								set @EstadoTx = 2
-							end
-						end
-						else
-						begin
-							if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec2) < @vCupoTec2)
-							begin
-								update tx_Asignacion
-										set Estado = 3
+								if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec2) < @vCupoTec2)
+								begin
+									update tx_Asignacion
+											set Estado = 3
+											Seccion = @vSeccionTec2
+											Fecha_Asignacion = GETDATE()
+										where Estudiante = @vEstudianteMenor
+
+										set @SeccionTX = @vSeccionTec1
+										set @EstadoTx = 1
+										set @vFecha_AsignacionTX = GETDATE()
+								end
+								else
+								begin
+									update tx_Asignacion
+									set Estado = 2
 										Seccion = @vSeccionTec2
-										Fecha_Asignacion = GETDATE()
+										Fecha_Asignacion = null
 									where Estudiante = @vEstudianteMenor
 
 									set @SeccionTX = @vSeccionTec1
 									set @EstadoTx = 1
 									set @vFecha_AsignacionTX = GETDATE()
-							end
-							else
-							begin
-								update tx_Asignacion
-								set Estado = 2
-									Seccion = @vSeccionTec2
-									Fecha_Asignacion = null
-								where Estudiante = @vEstudianteMenor
-
-								set @SeccionTX = @vSeccionTec1
-								set @EstadoTx = 1
-								set @vFecha_AsignacionTX = GETDATE()
+								end
 							end
 
+
+
+						end
 					end
-				end
-				else
-				begin
+					else
+					begin
+						set @vEstadoTx = 0
+					end
+				END
+				ELSE
+				BEGIN
 					if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec1) < @vCupoTec1)
 					begin
 						set @SeccionTX = @vSeccionAdmin
@@ -177,6 +189,7 @@ begin try
 							)
 						GROUP BY h.Estudiante
 						)
+
 						SELECT TOP 1 @vPromedioMenor = p.PromedioNotas, @vEstudianteMenor = p.Estudiante
 						FROM	PromedioNotasPorEstudiante p
 						ORDER BY p.PromedioNotas ASC;
@@ -240,7 +253,7 @@ begin try
 				begin
 					if(dbo.fncEstudianteCumplePrerrequisitos = 1)
 					begin
-						if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec1) = @vCupoAdmin)
+						if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionAdmin) = @vCupoAdmin)
 						begin
 							set @SeccionTX = @vSeccionAdmin
 							set @EstadoTx = 2
@@ -254,11 +267,12 @@ begin try
 					end
 					else
 					begin
+						set @EstadoTx = 0
 					end
 				end
 				else
 				begin
-					if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionTec1) = @vCupoAdmin)
+					if(dbo.fncObtenerCantidadEstudiantesSeccion(@vSeccionAdmin) = @vCupoAdmin)
 					begin
 						set @SeccionTX = @vSeccionAdmin
 						set @EstadoTx = 2
@@ -271,32 +285,6 @@ begin try
 					end
 				end
 			END
-
-			--Logica de asignacion
-			/* 1. A partir del CSV obtenemos el curso, con el curso, obtenemos el ID del tipo que pertence (Tecnico o Admin)
-			   2. Con ese ID comparamos si es tecnico o admin
-			   3. Ya comparado, realizamos un inner join entre seccion y curso para ver las secciones del curso seleccionado.
-			   4. (Para tecnico) El primer if devuelve dos registros, esos 2 registros son las secciones, tomamos el primer registro (seccion 1)
-			   y se toma el cupo, se hace un COUNT de tx_Asignacion en donde como condicion los registros tengan estado completado y contengan el
-			   ID de la seecion
-			   5. verificar que el cupo sea mayor que el resultado del count, si es mayor se inserta (hasta abajo) en tx_Asignacion, si el cupo no es mayor
-			   al count, se verifica el cupo de la segunda seccion. 
-			   6. Crear dos funciones (1 para el promedio) si el curso no tiene prerrequisito, se manda a llamar a la funcion que recibe como parametro
-			   el ID de la seccion y el ID del curso (dentro de la funcion se debe crear una variable decimal llamada promedio y una variable entera
-			   que se llame contador)
-			   7. Dentro de esa funcion crear un cursor que recorra tx_Asignacion, para todos los registros que pertenecen a esa seccion y que tiene
-			   estado aprobado.
-			   8. Por cada registro encontrado de tx_Asognacion hacer un inner join con la tabla estudiante para obtener la info del estudiante
-			   9. con el registro encontrado en la tabla estudiante hacer un inner join con la tabla historial cursos donde el ID_Curso sea igual
-			   al curso que se envio como parametro
-			   10. el registro encontrado tomar el atributo nota y sumarselo a promedio
-			   11. Incrementar contador
-			   12. al finalizar el cursor retornar promedio/contador
-			   13. Al obtener el promedio comparado su valor con 
-			   
-			*/
-
-
 
 		END
 		ELSE
