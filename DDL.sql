d1917c55e3efb7136927695e32b99cafefaabbc3
JoseMarold <jmmr114@gmail.com>
2023-11-11 18:44:33 -0600

    Modificacion archivo DDL

--- DDL.sql
+++ DDL.sql
@@ -75,31 +75,3 @@ constraint pk_curso_pp foreign key (Curso) references Curso (ID_Curso),
 constraint pk_curso_prerequisto foreign key (Prerrequisito) references Curso (ID_Curso)
 );
 
-
-/*
-OJO MIS REYES:
-estas dos tablas se están comentando porque vimos que se pueden simplificar a sólo una para ahorrarnos varias consultas
-
-la tabla Prerrequisito ahora tendría la siguiente estructura:
-id | id_curso | id_curso | estado_prerrequisito
-1 | 7 | 9 | 1 --> indica que el curso 7 tiene como prerrequisito al curso 9 y tiene estado activo (1)
-2 | 7 | 10 | 1 --> indica que el curso 7 tiene también como prerrequisito al curso 10 y tiene estado activo (1)
-2 | 3 | 2 | 0 --> indica que el curso 3 tuvo de prerrequisito en algún momento al curso 2 pero el estado está inactivo (0) .> eliminación lógica
-
-así con un montón xd
-*/
-create Table Prerrequisito(
-ID_Prerrequisito int PRIMARY KEY IDENTITY(1,1) not null,
-curso int not null
-constraint pk_curso_prerequisto foreign key (curso) references Curso (ID_Curso)
-);
-
---(Pendiente) Conectada a Curso y Prerrequisito
-create Table Curso_Prerrequisito(
-ID_Curso_Prerrequisito int PRIMARY KEY IDENTITY(1,1) not null,
-Curso int not null,
-Prerrequisto int not null
-constraint pk_curso_p foreign key (Curso) references Curso (ID_Curso),
-constraint pk_prerrequisto foreign key (Prerrequisto) references Prerrequisito
-);
-
